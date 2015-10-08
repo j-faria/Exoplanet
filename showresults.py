88e649423f53dd1ebcf4c22acaf5f630a86d9599
João Faria <joao.faria@astro.up.pt>
2015-10-08 14:22:10 +0100

    fail safe when there are few samples

--- showresults.py
+++ showresults.py
@@ -1,4 +1,15 @@
+import sys
+try:
+	options = sys.argv[1]
+except IndexError:
+	options = ''
+
+
 import postprocess
-postprocess.postprocess(cut=0.)
-import display
+logz_estimate, H_estimate, logx_samples, posterior_sample = postprocess.postprocess(cut=0.)
+if posterior_sample.shape[0] > 5:
+	import display
+	res = display.DisplayResults(options)
+else:
+	print 'Too few samples yet'
 
